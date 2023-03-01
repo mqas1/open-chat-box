ecb94fac1c468313cb373e7bc7d01c921d07ac0f
Ws538 <williamsu668@gmail.com>
2023-03-01 17:40:05 +1100

    added login functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,13 +1,14 @@
-const router = require('express').Router();
-const { User, Topic, Message } = require('../../models');
+const router = require("express").Router();
+const { User, Topic, Message } = require("../../models");
+const bcrypt = require("bcrypt");
 
 // The `/api/users` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all users
   try {
     const userData = await User.findAll({
-      include: [{ model: Topic }, { model: Message }]
+      include: [{ model: Topic }, { model: Message }],
     });
     res.status(200).json(userData);
   } catch (err) {
@@ -15,15 +16,47 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        user_name: req.body.user_name,
+      },
+    });
+    console.log(userData)
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    // Check if password matches
+    const isMatch = await bcrypt.compare(req.body.password, userData.password);
+    console.log(`password match is ${isMatch}`)
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid password" });
+    }
+
+    res.status(200).json({ success: true, message: "Login successful", user_name: userData.user_name});
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
   // find one user by their `id` value
   try {
     const userData = await User.findByPk(req.params.id, {
-      include: [{ model: Topic }, { model: Message }]
+      include: [{ model: Topic }, { model: Message }],
     });
 
     if (!userData) {
-      res.status(404).json({message: 'No user found with this id!'});
+      res.status(404).json({ message: "No user found with this id!" });
       return;
     }
 
@@ -33,7 +66,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new user
   /* req.body should look like this...
     {
