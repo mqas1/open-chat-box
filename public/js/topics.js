5a1349fd1e9160ed049b25fa60cb217d2683111e
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-03-04 17:10:54 +1100

    adds messages to topics

--- public/js/topics.js
+++ public/js/topics.js
@@ -1,8 +1,11 @@
+// const outputMessage = require("./chat");
+
 const createTopicBtn = document.getElementById("add-topic");
 const topicList = document.querySelector("#topic-list");
 const createTopic = document.getElementById("submitTopic");
 const newTopicInput = document.getElementById("newTopicInput");
 const showBtn = document.querySelector(".showInput");
+const chatMessages = document.querySelector(".chat-messages");
 // const socket = io();
 
 const getTopics = () => 
@@ -59,11 +62,48 @@ createTopic.addEventListener("click", function(event) {
   event.preventDefault();
   newTopic();
   hideNewTopicBtn();
-});    
+});
+
+const topicsListHandler = (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.dataset.id;
+    console.log(id);
+
+    fetch("/api/messages", {
+      method: "GET"
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data) {
+        console.log("no messages in this topic");
+      } else {
+        console.log(data);
+        const topicMessages = data.filter((message) => {
+          return message.topic_id === parseInt(id);
+        });
+        topicMessages.forEach((item) => renderMessages(item));
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+}
+
+const renderMessages = (message) => {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.setAttribute("data-id", message.id)
+  div.innerHTML = `<p class="user-time">${message.user.user_name}</p>
+  <p class="text">${message.content}</p>`;
+  chatMessages.appendChild(div);
+}
+
+topicList.addEventListener("click", topicsListHandler);
 
 const init = () => {
   topicList.innerHTML = "";
   getTopics().then((response) => response.forEach((item) => renderTopic(item)));
 };
-    
+
 init();
